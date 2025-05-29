414e4a2a2cc553b123ca4c6eb6aa77a41d8f9653
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-05-29 06:38:26 -0400

    Actualizar Creador.js

--- plugins2/Creador.js
+++ plugins2/Creador.js
@@ -1,13 +1,13 @@
 const handler = async (msg, { conn }) => {
-  const ownerNumber = "15167096032@s.whatsapp.net"; // Número del creador
+  const ownerNumber = "393715279301@s.whatsapp.net"; // Número del creador
   const ownerName = "Russell xz 🤖"; // Nombre visible del creador
 
   const messageText = `📞 *Contacto del Creador del Subbot:*
 
 Si tienes dudas, preguntas o sugerencias sobre el funcionamiento de *Azura Ultra Subbot*, puedes contactar a su creador.
 
-📌 *Nombre:* Russell
-📌 *Número:* +1 (516) 709-6032
+📌 *Nombre:* nadie 
+📌 *Número:* 393715279301
 💬 *Toca el contacto para enviarle un mensaje directo.`;
 
   // Enviar contacto vCard
